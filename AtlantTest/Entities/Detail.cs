467c7a4ab0bb62b65eeff6fe0e3965b190d86f69
Alexey Galyuk <linerath@mail.ru>
2018-05-04 12:46:22 +0300

    Added entities

---
+++ AtlantTest/Entities/Detail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtlantTest.Entities
+{
+    public class Detail
+    {
+        public int Id { get; set; }
+        public String NomenclatureCode { get; set; }
+        public String Name { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ AtlantTest/Entities/Storekeeper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AtlantTest.Entities
+{
+    public class Storekeeper
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String Patronymic { get; set; }
+    }
+}
