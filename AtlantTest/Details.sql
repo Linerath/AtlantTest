9eb827e4ba99cb0f1360ac801f09dde0a676d98a
Alexey Galyuk <linerath@mail.ru>
2018-05-06 23:24:18 +0300

    Added sql scripts

---
+++ AtlantTest/Details.sql
@@ -0,0 +1,35 @@
+USE [AtlantTest]
+GO
+
+INSERT INTO [dbo].[Details]
+           ([NomenclatureCode]
+           ,[Name]
+           ,[Quantity]
+           ,[CreationDate]
+           ,[DeleteDate]
+           ,[StorekeeperId])
+     VALUES
+           ('code0', 'name0', '1', '20180504 5:00:00 PM', NULL, 46),
+           ('code1', 'name1', '1', '20180504 5:01:00 PM', NULL, 47),
+           ('code2', 'name2', '2', '20180504 5:02:00 PM', NULL, 48),
+           ('code3', 'name3', '1', '20180504 5:03:00 PM', NULL, 49),
+           ('code4', 'name4', '1', '20180504 5:04:00 PM', NULL, 50),
+           ('code5', 'name5', '1', '20180504 5:05:00 PM', NULL, 51),
+           ('code6', 'name6', '1', '20180304 5:06:00 PM', NULL, 52),
+           ('code7', 'name7', '1', '20180504 5:07:00 PM', NULL, 53),
+           ('code8', 'name8', '1', '20180504 5:08:00 PM', NULL, 54),
+           ('code9', 'name9', '1', '20180504 5:09:00 PM', NULL, 55),
+           ('code10', 'name10', '1', '20180504 5:10:00 PM', NULL, 56),
+           ('code11', 'name11', '1', '20180504 5:11:00 PM', NULL, 57),
+           ('code12', 'name12', '1', '20180504 5:12:00 PM', NULL, 58),
+           ('code13', 'name13', '1', '20180504 5:13:00 PM', NULL, 59),
+           ('code14', 'name14', '1', '20180504 5:14:00 PM', NULL, 60),
+           ('code15', 'name15', '1', '20180504 5:15:00 PM', NULL, 61),
+           ('code16', 'name16', '1', '20180504 5:16:00 PM', NULL, 62),
+           ('code17', 'name17', '1', '20180504 5:17:00 PM', NULL, 63),
+           ('code18', 'name18', '1', '20180504 5:18:00 PM', NULL, 64),
+           ('code19', 'name19', '1', '20180504 5:19:00 PM', NULL, 65),
+           ('code20', 'name20', '1', '20180504 5:20:00 PM', NULL, 66)
+GO
+
+
---
+++ AtlantTest/Storekeepers.sql
@@ -0,0 +1,30 @@
+USE [AtlantTest]
+GO
+
+INSERT INTO [dbo].[Storekeepers]
+           ([Name]
+           ,[Surname]
+           ,[Patronymic])
+     VALUES
+           ('name0', 'surname0', 'patronymic0'),
+           ('name1', 'surname1', 'patronymic1'),
+           ('name2', 'surname2', 'patronymic2'),
+           ('name3', 'surname3', 'patronymic3'),
+           ('name4', 'surname4', 'patronymic4'),
+           ('name5', 'surname5', 'patronymic5'),
+           ('name6', 'surname6', 'patronymic6'),
+           ('name7', 'surname7', 'patronymic7'),
+           ('name8', 'surname8', 'patronymic8'),
+           ('name9', 'surname9', 'patronymic9'),
+           ('name10', 'surname10', 'patronymic10'),
+           ('name11', 'surname11', 'patronymic11'),
+           ('name12', 'surname12', 'patronymic12'),
+           ('name13', 'surname13', 'patronymic13'),
+           ('name14', 'surname14', 'patronymic14'),
+           ('name15', 'surname15', 'patronymic15'),
+           ('name16', 'surname16', 'patronymic16'),
+           ('name17', 'surname17', 'patronymic17'),
+           ('name18', 'surname18', 'patronymic18'),
+           ('name19', 'surname19', 'patronymic19'),
+           ('name20', 'surname20', 'patronymic20')
+GO
